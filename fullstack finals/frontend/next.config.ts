dff6b83688f67233c0cef64f626dcc676df43340
zartasha-khan123 <zartashimran12345@gmail.com>
2025-07-12 03:15:47 +0500

    d

--- fullstack finals/frontend/next.config.ts
+++ fullstack finals/frontend/next.config.ts
@@ -1,7 +1,16 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
 };
 
 export default nextConfig;
