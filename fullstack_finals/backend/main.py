701bf1b1872485b695f18fd5e337905afd097f86
zartasha-khan123 <zartashimran12345@gmail.com>
2025-07-20 05:23:02 +0500

    d

--- fullstack_finals/backend/main.py
+++ fullstack_finals/backend/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 # ✅ Allow requests from frontend (localhost:3000)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://assignment2-sir-aneeq-healthwealtha-alpha.vercel.app","http://localhost:3000","https://def9ceffcdfe.ngrok-free.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
